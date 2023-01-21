f8b8a27cdab7c4d71761b58200350e5f7765721d
Your Name <you@example.com>
2023-01-21 16:29:47 -0700

    Established homepage/GetEntries function; Added necessary headers to other pages

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,33 +1,62 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
-
 import axios from "axios";
 
 const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
-  //TODO: Add an AddCars Page to add a car for a logged in user's garage
   const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+  const [entries, setEntries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    const fetchCars = async () => {
+    getEntries();
+  }, [token]);
+
+    async function getEntries() {
       try {
-        let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
+        let response = await axios.get("http://127.0.0.1:8000/api/capstone/addEntry/", entries, {
           headers: {
             Authorization: "Bearer " + token,
           },
         });
-        setCars(response.data);
+        console.log(response.data);
+        setEntries(response.data);
       } catch (error) {
         console.log(error.response.data);
       }
-    };
-    fetchCars();
-  }, [token]);
+    }
+    
+  function handleSubmit(event) {
+    event.preventDefault();
+    getEntries();
+  }
+
   return (
-    <div className="container">
+    <>
+      <div className="title-container">
+        <h1>Home Page for {user.username}!</h1>
+      </div>
+      <div className="search-bar">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.target.value)}
+            />
+            <button className="button" type="submit">
+              Search
+            </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
+
+{/* <div className="container">
       <h1>Home Page for {user.username}!</h1>
       {cars &&
         cars.map((car) => (
@@ -35,8 +64,4 @@ const HomePage = () => {
             {car.year} {car.model} {car.make}
           </p>
         ))}
-    </div>
-  );
-};
-
-export default HomePage;
+    </div> */}
